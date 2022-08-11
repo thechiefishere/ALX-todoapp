164c540354c4f400b3ed5f00b07afe12af7b2c61
John <john.toriola.tj@gmail.com>
2022-08-11 20:28:32 +0100

    added migration and completed flag

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-from flask import Flask, render_template, request, jsonify
+from email.policy import default
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 import sys
+from flask_migrate import Migrate
 
 load_dotenv()
 app = Flask(__name__)
 db_password = os.getenv('MY_PASSWORD')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://jbabajohn:{db_password}@localhost:5432/todoapp'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
-
-db.create_all()
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -39,6 +41,19 @@ def create_todo():
         db.session.close()
     if not error:
         return jsonify(body)
+    
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.debug = True
